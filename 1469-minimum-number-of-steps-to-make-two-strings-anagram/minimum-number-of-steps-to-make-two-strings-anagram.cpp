133b50427d5e6c4be2d97fcd1fdcca52ad5d26bd
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-01-13 16:52:52 +0530

    Time: 46 ms (86.44%) | Memory: 16.9 MB (82.29%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        vector<int> vs(26,0);
-        vector<int> vt(26,0);
-        for(int i=0; i < s.length() ; i++ ) {
-            vs[s[i]-'a']++;
-            vt[t[i]-'a']++;
-        }
-        int ans = 0 , res1 = 0, res2 = 0;
-        for(int i = 0 ; i < 26 ; i++) {
-            int sub = min(vs[i],vt[i]);
-            res1 += (vs[i] - sub);
-            res2 += (vt[i] - sub);
-        }
-
-        return max(res1,res2);
+        int vs[26] ={0} , ans = 0;
+        for(int i = 0; i < s.length() ; i++ ) vs[s[i]-'a']++ , vs[t[i]-'a']--;
+        for(int i = 0 ; i < 26 ; i++) ans += abs(vs[i]);
+        return ans/2;
     }
 };
