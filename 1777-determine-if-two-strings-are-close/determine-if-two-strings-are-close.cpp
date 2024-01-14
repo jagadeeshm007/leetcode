e6f187eb1a687155b952abe6a88e4f943f7dde9a
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-01-14 12:26:48 +0530

    Time: 102 ms (63.65%) | Memory: 21.3 MB (16.73%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
+        //speeds up code , dont know whyyy  :)
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+
+            // my code starts here
         if(word1.length() != word2.length() ) return false;
         vector<int> a(26,0),b(26,0) ;
         set<char> s1,s2;
