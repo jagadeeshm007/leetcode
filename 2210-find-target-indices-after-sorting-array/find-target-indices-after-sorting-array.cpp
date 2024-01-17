125d7963b7e172654c3b6f1e394d9ecf2fe44e81
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-01-17 11:24:16 +0530

    Time: 4 ms (70.87%) | Memory: 12 MB (65.55%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
@@ -3,10 +3,11 @@ public:
     vector<int> targetIndices(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         vector<int> c;
-        if(binary_search(nums.begin(),nums.end(),target)){
         int k= lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        if(k!=nums.size() && nums[k]==target){
         for(int i=k;i<nums.size();i++) {
             if(nums[i]==target) c.push_back(i);
+            else break;
         }
         }
         else return {};
