5f056042df959ceb9f47874649e77e12c0719e93
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-01-17 15:23:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (69.36%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> mp;
+        for(auto i : arr) {
+            mp[i]++;
+        }
+        unordered_set<int> s;
+        for(auto i : mp){
+            if(s.find(i.second) != s.end()) return false;
+            s.insert(i.second);
+        }
+        return true;
+    }
+};
