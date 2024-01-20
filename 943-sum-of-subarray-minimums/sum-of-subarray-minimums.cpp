92e2a1ae6d519fd0be1ca7a2a734341441aadc06
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-01-20 23:48:58 +0530

    Time: 56 ms (97.47%) | Memory: 41.9 MB (79.46%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int MOD = 1000000007;
+    int sumSubarrayMins(vector<int>& arr) {
+        stack<int> st;
+        long long result = 0;
+        for (int i = 0; i <= arr.size(); i++) {
+            while (!st.empty()&&(i==arr.size()||arr[st.top()]>=arr[i])){
+                int m = st.top();
+                st.pop();
+                int left = -1 , right = i;
+                if(!st.empty()) left = st.top();
+                long long c = (m - left) * (right - m) % MOD;
+                result = ( result + (c * arr[m]) % MOD) % MOD;
+            }
+            st.push(i);
+        }
+        return (int)(result);
+    }
+};
