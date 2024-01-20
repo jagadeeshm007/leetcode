fec7216f7eb7d5122402037b5e35b0908175fcce
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-01-20 23:51:02 +0530

    Time: 37 ms (99.90%) | Memory: 41.7 MB (80.43%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,3 +1,9 @@
+static const int _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
 class Solution {
 public:
     int MOD = 1000000007;
