4241076b588b9397b1a7cdf066626794c90e0eac
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-01-22 22:55:36 +0530

    Time: 40 ms (99.88%) | Memory: 63.2 MB (24.87%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
         for(int i = 0 ; i < nums.size() ; i++ ){
             if(nums[abs(nums[i])-1]<0){
                 return abs(nums[i]);
