56573e93121a0c093b95fd1c126c5c76c6f9ce7c
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-01-22 22:12:45 +0530

    Time: 19 ms (89.24%) | Memory: 23.8 MB (36.81%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,15 +1,48 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> a(n+1,0);
-        int b = 0,c=0,v=0;
-        for(int i = 1 ; i <= n ; i++ ) { 
-            if(a[nums[i-1]]==1) v=nums[i-1];
-            b^=nums[i-1];
-            c^=i;
-            a[nums[i-1]]++;
+        int n = nums.size() ;
+        int numsxor = 0, nxor = 0;
+        for(int i = 1 ; i <= n ; i++ ) {
+            numsxor^=nums[i-1] ;
+            nxor^=i ;
         }
-        return {v,(b^c^v)};
+        int Xor = numsxor ^ nxor;
+
+        cout<<Xor<<"\n";
+
+        int lsb = 0;
+        while( !(Xor & (1 << lsb)) ) {
+            lsb++;
+        }
+        cout<<lsb<<"\n";
+        int numszero = 0, numsone = 0;
+        int nzero = 0, none = 0;
+        for(int i = 0 ; i < n ; i++ ) { 
+            if(nums[i] & 1<<lsb) {
+                numsone ^= nums[i];
+            }
+            else {
+                numszero ^= nums[i];
+            }
+        }
+        for(int i = 1 ; i <= n ; i++ ) { 
+            if(i & 1<<lsb) {
+                none ^= i;
+            }
+            else {
+                nzero ^= i;
+            }
+        }
+        int v1 = numszero ^ nzero ;
+        int v2 = numsone ^ none ;
+
+        for(int i = 0 ; i < n ; i++ ) {
+            if( nums[i] == v1 ) {
+                return {v1,v2};
+            }
+        }
+
+        return {v2,v1};
     }
 };
