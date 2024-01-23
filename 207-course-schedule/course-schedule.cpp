318451d0151c3bf025de91a83c48b9c2b31a2b99
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-01-23 23:35:46 +0530

    Time: 19 ms (57.86%) | Memory: 17.6 MB (8.36%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>res(n);
+        vector<int>deg(n,0);
+        for(auto i:prerequisites)
+        {
+            res[i[1]].push_back(i[0]);
+            deg[i[0]]++;
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++)
+        {
+            if(deg[i]==0)q.push(i);
+        }
+        
+        if(q.empty())return {};
+        vector<int>ans;
+        while(!q.empty())
+        {
+            auto a=q.front();
+            ans.push_back(a);
+            q.pop();
+            for(auto k:res[a])
+            {
+                deg[k]--;
+                if(deg[k]==0)q.push(k);
+            }
+        }
+
+        for(int i=0;i<n;i++)if(deg[i])return 0;
+        return 1;
+    }
+};
