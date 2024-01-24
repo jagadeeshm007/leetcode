b96abab403d8351331df5c2eb88337595e6d4760
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-01-24 23:37:12 +0530

    Time: 234 ms (96.25%) | Memory: 176.9 MB (99.73%) - LeetSync

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -12,22 +12,15 @@
 class Solution {
 public:
     int c=0;
-    int gen(int pp,int mask){
-        return (pp & mask) ? (pp & ~mask) : (pp | mask);
-    }
-    int valid(int pp){
-        int bits =  __builtin_popcount(pp);
-        return (bits==1 || bits==0) ? 1 : 0;
-    }
     void dfs(TreeNode* root ,int pp){
         if (!root) return;
-        pp = gen( pp , 1 << root->val );
+        pp ^= 1 << root->val;
         if (!root->left && !root->right) {
-            c += valid(pp);
+            c += (!pp or !(pp-1 & pp));
             return;
         }
-        if (root->left) dfs(root->left,pp);
-        if (root->right) dfs(root->right,pp);
+        dfs(root->left,pp);
+        dfs(root->right,pp);
     }
     int pseudoPalindromicPaths (TreeNode* root) {
         dfs(root,0);
