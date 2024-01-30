75d22b75d0df3e312d2b1370f4509193241e1b6c
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-01-30 18:19:06 +0530

    Time: 3 ms (41.54%) | Memory: 8.1 MB (9.76%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    void Fun(int ind,int n, string &digits,string &s,vector<string>&dp,map<char,string>&mp){
+    map<char,string>mp = {{'2',"abc"} ,{'3',"def"} ,{'4',"ghi"} , {'5',"jkl"} ,{'6',"mno"} ,{'7',"pqrs"},{'8',"tuv"},{'9',"wxyz"} };
+    void Fun(int ind,int n, string &digits,string s,vector<string>&dp){
         if(ind==n){
             dp.push_back(s);
             return;
         }
             for(auto it:mp[digits[ind]]){
-                s.push_back(it);
-                Fun(ind+1,n,digits,s,dp,mp);
-                s.pop_back();
-            
+                Fun(ind+1,n,digits,s+it,dp);
         }
     }
     vector<string> letterCombinations(string digits) {
 
         if(digits.size()==0)return{};
-        map<char,string>mp = {{'2',"abc"} ,{'3',"def"} ,{'4',"ghi"} , {'5',"jkl"} ,{'6',"mno"} ,{'7',"pqrs"},{'8',"tuv"},{'9',"wxyz"} };
         vector<string>dp;
         string s;
-        Fun(0,digits.size(),digits,s,dp,mp);
+        Fun(0,digits.size(),digits,s,dp);
         return dp;
     }
 };
