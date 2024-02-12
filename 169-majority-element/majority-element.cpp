a5b5fb8cb5f75ef4a8758de5dcac6b826d8f53b0
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-02-12 17:57:07 +0530

    Time: 3 ms (99.85%) | Memory: 21.9 MB (33.81%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
-    
+    //Boyer-Moore Majority Vote Algorithm O(N) & O(1)
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return nums[nums.size()/2];
+        int c = 1;
+        int n = nums[0];
+        for(int i = 1 ; i < nums.size() ; i ++ ) {
+            if( n == nums[i] ) c++;
+            else c--;
+            if(c == 0) {n = nums[i];c = 1;}
+        }
+        return n;
     }
 };
