c92ca2ee92fa5847b6f728eef1164a3893b4af4e
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-02-20 17:04:45 +0530

    Time: 10 ms (88.91%) | Memory: 20.5 MB (16.85%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -5,3 +5,10 @@ public:
         return (n*(n+1))/2 - accumulate(nums.begin(),nums.end(),0);
     }
 };
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
