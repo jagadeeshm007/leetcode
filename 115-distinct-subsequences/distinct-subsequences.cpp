fc3e14f03c935b20a1b7357eb8f5242304ebb0f5
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-02-22 12:04:05 +0530

    Time: 44 ms (27.11%) | Memory: 16.4 MB (76.20%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     long long mod = 1e9+7;
     long long dp[1001][1001];
-    int mia(int i,string s,string t,int m) {
+    int mia(int i,string &s,string &t,int m) {
         if(m>=t.size()) return 1;
         if(dp[i][m]!=-1) return dp[i][m];
         if(i>=s.size()) return 0;
 
         long long n=0;
-        
+
         n = (n % mod + mia(i+1,s,t,m) % mod) % mod;
         if(s[i] == t[m]) n = ( n % mod + mia(i+1,s,t,m+1) % mod) % mod;        
         return dp[i][m] =( n % mod);
@@ -17,4 +17,5 @@ public:
        memset(dp,-1,sizeof(dp));
       return mia(0,s,t,0);
     }
+    
 };
