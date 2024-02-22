184b83613b948d67f3571549b2054a8271eb6108
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-02-22 12:02:02 +0530

    Time: 2291 ms (5.01%) | Memory: 413.5 MB (13.37%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -3,21 +3,14 @@ public:
     long long mod = 1e9+7;
     long long dp[1001][1001];
     int mia(int i,string s,string t,int m) {
-        if(m>=t.size()){
-            return 1;
-        }
-        if(dp[i][m]!=-1) {
-            return dp[i][m];
-        }
-        if(i>=s.size()) {
-            return 0;
-        }
+        if(m>=t.size()) return 1;
+        if(dp[i][m]!=-1) return dp[i][m];
+        if(i>=s.size()) return 0;
+
         long long n=0;
-        if(s[i] == t[m]) {
-            n =( n % mod + mia(i+1,s,t,m+1) % mod) % mod;
-        }
-        n = (n % mod + mia(i+1,s,t,m) % mod) % mod;
         
+        n = (n % mod + mia(i+1,s,t,m) % mod) % mod;
+        if(s[i] == t[m]) n = ( n % mod + mia(i+1,s,t,m+1) % mod) % mod;        
         return dp[i][m] =( n % mod);
     }
     int numDistinct(string s, string t) {
