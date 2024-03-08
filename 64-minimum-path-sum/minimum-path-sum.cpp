dce31f39c6c5416358554424e3d302232397f992
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-03-08 15:20:44 +0530

    Time: 3 ms (95.73%) | Memory: 12 MB (78.66%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -5,14 +5,16 @@ public:
         int m = grid[m].size();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (i - 1 < 0 and j - 1 < 0) {
-                    continue;
+                if (i <= 0 and j <= 0) {
+                    grid[i][j] = grid[i][j];
                 }
-                if (i - 1 >= 0 and j - 1 < 0) {
+                else if (j <= 0) {
                     grid[i][j] += grid[i - 1][j];
-                } else if (j - 1 >= 0 and i - 1 < 0) {
+                } 
+                else if (i <= 0) {
                     grid[i][j] += grid[i][j - 1];
-                } else {
+                } 
+                else {
                     grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
                 }
             }
