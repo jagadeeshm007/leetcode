c57e679f73b9f2326d087ff1d45e6532cf7e6ab8
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-03-08 12:59:55 +0530

    Time: 7 ms (31.73%) | Memory: 11 MB (70.87%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,38 +1,14 @@
 class Solution {
 public:
-    long long minmin(vector<vector<int>>& triangle, int n, int j,vector<vector<int>> &dp) {
-        
-        if (n <= 0) {
-            return  triangle[0][0];
-        }
-        if(dp[n][j]!=INT_MAX){
-            return dp[n][j];
-        }
-        long long p = 1e5, q = 1e5;
-        int m = triangle[n].size();
-        if (j>0) {
-            p = min( p , triangle[n][j] + minmin(triangle, n - 1, j - 1,dp));
-        }
-        if (j<m-1) {
-            q = min(q , triangle[n][j] + minmin(triangle, n - 1, j,dp));
-        }
-        return  dp[n][j] = min(p, q);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
-        long long p = 1e5, q = 1e5;
-        int n = triangle.size() - 1;
-        int m = triangle[n].size() ;
-        if(n==0 and m==1) return triangle[0][0];
-        vector<vector<int>> dp(n+2,vector<int>(m+1,INT_MAX));
+        int n = triangle.size() ;
         
-        for (int j = 0; j < m; j++) {
-            if (j>0) {
-                p = min(p,triangle[n][j] + minmin(triangle, n - 1, j - 1,dp));
-            }
-            if (j<m-1) {
-                q = min(q,triangle[n][j] + minmin(triangle, n - 1, j,dp));
+        for (int j = n-2;j>=0;j--) {
+            int m = triangle[j].size();
+            for(int i=0;i<m;i++){
+                triangle[j][i]+=min(triangle[j+1][i],triangle[j+1][i+1]);
             }
         }
-        return min(p, q);
+        return triangle[0][0];
     }
 };
