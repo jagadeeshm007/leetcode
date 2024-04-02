6d5db3ac7c07c530aa354b88481f9405e4e893c6
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-04-02 10:11:30 +0530

    Time: 3 ms (91.66%) | Memory: 8.2 MB (85.95%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if(s.length() != t.length()){
-            return false;
-        }
         int map[257] , visit[257];
-        
         for(int i = 0 ; i<s.length();i++){
             if( map[s[i]] == 0) {
                 if(visit[t[i]] == 0) {
