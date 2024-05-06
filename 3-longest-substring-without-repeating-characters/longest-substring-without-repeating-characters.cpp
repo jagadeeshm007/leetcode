5a694e2c3c137371b0da5e7467ee735311d18097
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-05-06 12:12:53 +0530

    Time: 6 ms (85.17%) | Memory: 9.9 MB (84.83%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        int presentSize = 0, maxSize = 0, size = s.length(), prev = 0;
         int frq[129];
-        memset(frq,-1,sizeof(frq));
-        int ans=0,mx=0, n= s.length(),prev=0;
-        for(int i=0;i<n;i++){
-            if(frq[s[i]] == -1){
+        memset(frq, -1, sizeof(frq));
+        for (int i = 0; i < size; i++) {
+            if (frq[s[i]] == -1) {
                 frq[s[i]] = i;
-                ans++;
-            }
-            else{
-                while(prev <= frq[s[i] ]){
+                presentSize++;
+            } else {
+                while (prev <= frq[s[i]]) {
                     frq[s[prev]] = -1;
                     prev++;
-                    ans--;
+                    presentSize--;
                 }
-                frq[s[i] ] = i;
-                ans++;
+                frq[s[i]] = i;
+                presentSize++;
             }
-            mx = max(mx,ans);
-            //debugging print statement
-            //cout<<s[i]<<" "<<frq[s[i]]<<" "<<ans<<" "<<mx<<" "<<prev<<endl;
+            maxSize = max(maxSize, presentSize);
+            // debugging print statement
+            // cout<<s[i]<<" "<<frq[s[i]]<<" "<<presentSize<<" "<<maxSize<<" "<<prev<<endl;
         }
-        return mx;
+        return maxSize;
     }
 };
