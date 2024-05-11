d3f0c56a46492bf665b7b95fc32ab907bbf671c0
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-05-11 22:28:46 +0530

    Time: 549 ms (10.56%) | Memory: 51.8 MB (13.20%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int dp[10000];
+    int find(vector<int>& nums,int i,int n){
+        if(i>=n)
+            return 1;
+        if(dp[i]!=-1)
+            return dp[i];
+        int p=0;
+        for(int j=i+1;j<=i+nums[i];j++){
+            if(j<=n) p = max(p,find(nums,j,n));
+            if(p) break;
+        }
+        return dp[i]=p;
+    }
+    bool canJump(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        int n = nums.size();
+        return find(nums,0,n-1);
+    }
+};
