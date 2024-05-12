3d2f1027c6af8a3b5a77fe4325c65cd929f57b35
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-05-12 15:10:43 +0530

    Time: 6 ms (19.62%) | Memory: 10.2 MB (59.49%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int path(vector<vector<int>>& obstacleGrid,int i,int j,int n,int m,vector<vector<int>>& dp) {
+        if(i>=n-1 && j>=m-1 && obstacleGrid[i][j] == 0) 
+            return 1;
+            
+        if(obstacleGrid[i][j] == 1) 
+            return 0;
 
-        if(i>=n-1 && j>=m-1 && obstacleGrid[i][j] == 0) return 1;
-        if(obstacleGrid[i][j] == 1) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
+        if(dp[i][j]!=-1) 
+            return dp[i][j];
+
         int p=0,q=0;
-        if(i+1<n && obstacleGrid[i+1][j] == 0) p = path(obstacleGrid,i+1,j,n,m,dp);
-        if(j+1<m && obstacleGrid[i][j+1] == 0) q = path(obstacleGrid,i,j+1,n,m,dp);
+        if(i+1<n && obstacleGrid[i+1][j] == 0) 
+            p = path(obstacleGrid,i+1,j,n,m,dp);
+
+        if(j+1<m && obstacleGrid[i][j+1] == 0) 
+            q = path(obstacleGrid,i,j+1,n,m,dp);
         
         return dp[i][j] = p+q;
     }
