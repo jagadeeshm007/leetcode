b6a387f1046ac7eaa24c7ae2e1519c25cd84d996
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2024-05-12 23:06:38 +0530

    Time: 3 ms (99.39%) | Memory: 11.1 MB (64.23%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,43 @@
+#define SIZE 100000 + 1
+int p[SIZE * 2];
+
+class Solution {
+public:
+    string convert(string s) {
+        string newString = "@";
+        for (int i = 0; i < s.size(); i++)
+            newString += "#" + s.substr(i, 1);
+        newString += "#$";
+        return newString;
+    }
+    string longestPalindrome(string s) {
+        string cp = s;
+        s = convert(s);
+        int n = s.size()-2;
+        vector<int> p(n + 2);
+        int l = 1, r = 1;
+        for(int i = 1; i <= n; i++) {
+            p[i] = max(0, min(r - i, p[l + (r - i)]));
+            while(s[i - p[i]] == s[i + p[i]]) {
+                p[i]++;
+            }
+            if(i + p[i] > r) {
+                l = i - p[i], r = i + p[i];
+            }
+        }
+        int mx=-1,it=-1;
+        for(int i=0;i<n;i++) {
+            if(mx<p[i]){
+                mx=p[i];
+                it=i;
+            }
+        }
+        string q;
+        for(int i=it-mx+1;i<it+mx;i++){
+            cout<<s[i];
+            if(s[i]!='#') q+=s[i];
+        }
+
+        return q;
+    }
+};
