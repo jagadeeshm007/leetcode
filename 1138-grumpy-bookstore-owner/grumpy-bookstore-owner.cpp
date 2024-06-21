19e731892e546c1499ec9e5ecfb225482a2f150f
Jagadeesh <96563707+jagadeeshm007@users.noreply.github.com>
2024-06-21 11:31:37 +0530

    Time: 17 ms (91.47%) | Memory: 34.4 MB (47.97%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int mx=0,fin=0;
-        int sm=0,c=0;
-        for(int i=0;i<minutes;i++){
-            if(grumpy[i]){
-                sm+=customers[i];
-            }
-            else{
-                fin+=customers[i];
-            }
+        int sm = 0, c = 0, fin = 0;
+        for (int i = 0; i < minutes; i++) {
+            if (grumpy[i])
+                sm += customers[i];
+            else
+                fin += customers[i];
         }
-       mx=sm;
-        for(int i=minutes;i<grumpy.size();i++){
-            if(grumpy[i]){
-                if(grumpy[i-minutes]) sm-=customers[i-minutes];
-                sm+=+ customers[i];
-            }
-            else{
-                if(grumpy[i-minutes]) sm-=customers[i-minutes];
-                fin+=customers[i];
-            }
-            mx = max(mx,sm);
+        int mx = sm;
+        for (int i = minutes; i < grumpy.size(); i++) {
+            if (grumpy[i - minutes])
+                sm -= customers[i - minutes];
+            if (grumpy[i])
+                sm += customers[i];
+            else
+                fin += customers[i];
+            mx = max(mx, sm);
         }
 
-        return fin+mx;
+        return fin + mx;
     }
 };
