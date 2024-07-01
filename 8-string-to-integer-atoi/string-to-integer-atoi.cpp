ca8546fe5bf9c84ff848cf6dcda9f49093b4bd4b
Jagadeesh <96563707+jagadeeshm007@users.noreply.github.com>
2024-07-01 13:41:28 +0530

    Time: 3 ms (58.15%) | Memory: 8.2 MB (66.74%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,44 +1,25 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int num=0,i=0;
-        int mx = INT_MAX, mi = INT_MIN;
-        int d=0,sg=0;
+        int num = 0, i = 0;
+        int sign = 1;
+        while (i < s.length() && s[i] == ' ') i++;
 
-        while(i<s.length() && s[i]==' ') i++;
-
-        for(i;i<s.length();i++) {
-            int digit = 0;
-            if(isdigit(s[i])){
-                digit = s[i] - '0';
-                d++;
-            }
-            else{
-                if(sg==0 && d==0){
-                    if(s[i] == '-') sg = -1;
-                    else if(s[i] == '+') sg = 1;
-                    else break;
-                }
-                else break;
-            }
-            
-            // HANDLE OVERFLOW
-            if((sg==1 || sg==0) && num > INT_MAX / 10 || (num == INT_MAX / 10 && digit > INT_MAX % 10)){
-                if(sg==-1){
-                    if(sg*num == INT_MIN / 10 && sg*digit <= INT_MIN % 10){
-                        return INT_MIN;
-                    }
-                }
-                return INT_MAX;
-            }
-            //HANDLE UNDERFLOW
-            if((sg ==-1) && sg*num < INT_MIN / 10 || (sg*num == INT_MIN / 10 && sg*digit < INT_MIN % 10)){
-                return INT_MIN;
-            }
-
-            num = num*10 + digit;
+        if (i < s.length() && (s[i] == '-' || s[i] == '+')) {
+            sign = (s[i] == '-') ? -1 : 1;
+            i++;
         }
-        if(sg==0) sg=1;
-        return sg*num;
+        while (i < s.length() && isdigit(s[i])) {
+            int digit = s[i] - '0';
+            
+            if (num > (INT_MAX - digit) / 10) {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+
+            num = num * 10 + digit;
+            i++;
+        }
+
+        return num * sign;
     }
 };
