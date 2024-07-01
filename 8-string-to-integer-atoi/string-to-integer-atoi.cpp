15b4c6aab3783e0581da33911016a9c623a29a72
Jagadeesh <96563707+jagadeeshm007@users.noreply.github.com>
2024-07-01 13:38:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (66.74%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        //if(s == "-2147483648") return INT_MIN;
-        //if(s == "2147483647") return INT_MAX;
         int num=0,i=0;
         int mx = INT_MAX, mi = INT_MIN;
         int d=0,sg=0;
+
         while(i<s.length() && s[i]==' ') i++;
+
         for(i;i<s.length();i++) {
             int digit = 0;
             if(isdigit(s[i])){
@@ -19,13 +19,11 @@ public:
                     else if(s[i] == '+') sg = 1;
                     else break;
                 }
-                else{
-                    break;
-                }
+                else break;
             }
+            
             // HANDLE OVERFLOW
             if((sg==1 || sg==0) && num > INT_MAX / 10 || (num == INT_MAX / 10 && digit > INT_MAX % 10)){
-                //cout<<sg<<num<<" "<<sg*digit <<" "<< INT_MIN % 10<<" "<<(sg*num == INT_MIN / 10 && sg*digit <= INT_MIN % 10)<<" -> ";
                 if(sg==-1){
                     if(sg*num == INT_MIN / 10 && sg*digit <= INT_MIN % 10){
                         return INT_MIN;
