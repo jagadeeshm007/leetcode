82f127055649313869c8586a4e622ccebe5274e6
Jagadeesh <96563707+jagadeeshm007@users.noreply.github.com>
2024-07-01 18:25:37 +0530

    Time: 886 ms (6.77%) | Memory: 169 MB (25.51%) - LeetSync

---
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    int c=0;
+    void marker(int i,int j,vector<vector<int>>& grid,int mrk){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid.size() || grid[i][j]!=1){
+            return;
+        }
+        grid[i][j] = 0;
+        c++;
+        marker(i+1,j,grid,mrk);
+        marker(i-1,j,grid,mrk);
+        marker(i,j+1,grid,mrk);
+        marker(i,j-1,grid,mrk);
+        grid[i][j] = mrk;
+    }
+    int largestIsland(vector<vector<int>>& grid) {
+        map<int,int> grp;
+        grp[0]=0;
+        int n = grid.size();
+        int mrk=2;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                if(grid[i][j] == 1){
+                    c=0;
+                    marker(i,j,grid,mrk);
+                    grp[mrk] = c;
+                    mrk++;
+                }
+            }
+        }
+        cout<<grp.size()<<endl;
+        if(grp.size()==1) return 1;
+        if(grp.size()==2){
+            if(grp[2]==(n*n))
+                return grp[2];
+            return grp[2]+1;
+        }
+
+        int ans=0;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                int tmp=1;
+                unordered_set<int> s;
+                if(grid[i][j] == 0){
+                    if(i-1>=0) s.insert(grid[i-1][j]);
+                    if(i+1<n) s.insert(grid[i+1][j]);
+                    if(j-1>=0) s.insert(grid[i][j-1]);
+                    if(j+1<n) s.insert(grid[i][j+1]);
+                    for(auto it : s){
+                        cout<<it<<" ";
+                        tmp+=grp[it];
+                    }
+                    cout<<endl;
+                }
+                ans = max(ans,tmp);
+            }
+        }
+
+        // for(auto it:grid){
+        //     for(auto i:it){
+        //         cout<<i<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        // for(int i=2;i<mrk;i++) cout<<i<<" -> "<<grp[i]<<endl;
+
+        return ans;
+    }
+};
