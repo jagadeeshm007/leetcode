f067e432591d6281ceba1e01b79f21a327d22fe6
Jagadeesh <96563707+jagadeeshm007@users.noreply.github.com>
2024-07-03 12:20:23 +0530

    Time: 69 ms (56.53%) | Memory: 38.7 MB (80.72%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -4,10 +4,10 @@ public:
         int n = nums.size();
         if(n < 5) return 0;
         sort(begin(nums),end(nums));
-        int case0 = abs(nums[0]-nums[n-4]);
-        int case1 = abs(nums[1]-nums[n-3]);
-        int case2 = abs(nums[2]-nums[n-2]);
-        int case3 = abs(nums[3]-nums[n-1]);
+        int case0 = nums[n-4] - nums[0];
+        int case1 = nums[n-3] - nums[1];
+        int case2 = nums[n-2] - nums[2];
+        int case3 = nums[n-1] - nums[3];
         return min({case0,case1,case2,case3});
     }
 };
