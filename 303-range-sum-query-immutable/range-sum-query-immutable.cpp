383a176cddec4ea58972a62bfad195f25148002a
Jagadeesh <96563707+jagadeeshm007@users.noreply.github.com>
2024-07-04 16:44:56 +0530

    Time: 15 ms (84.35%) | Memory: 20.7 MB (47.99%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -1,13 +1,17 @@
 class NumArray {
 public:
-    vector<int> seg;
-    int n;
-public:
     NumArray(vector<int>& nums) {
         n=nums.size();
         seg.resize(4*n,0);
         buildTree(0,n-1,0,nums);
     }
+    int sumRange(int left, int right) {
+        return query(left,right,0,n-1,0);
+    }
+
+private:
+    vector<int> seg;
+    int n;
     void buildTree(int left,int right,int idx,vector<int>& nums) {
         if(left == right) {
             seg[idx] = nums[left];
@@ -20,19 +24,12 @@ public:
     }
     int query(int left, int right,int start,int end,int idx){
         // Completely within the query range
-        if(left<=start && end<=right) {
-            return seg[idx];
-        }
+        if(start>=left && right>=end) return seg[idx];
         // Completely out of the query range
-        if(left>end || right<start) {
-            return 0;
-        }
+        if(left>end || start>right ) return 0;
         // Partially within the query range
         int mid = start + (end - start)/2;
         return query(left,right,start,mid,idx*2+1) + query(left,right,mid+1,end,idx*2+2);
-    }
-    int sumRange(int left, int right) {
-        return query(left,right,0,n-1,0);
     }
 };
 
