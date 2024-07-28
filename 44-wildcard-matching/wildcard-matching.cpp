f390520631e0c2f95f99d293d97eeb34cc706384
Jagadeesh <96563707+jagadeeshm007@users.noreply.github.com>
2024-07-28 15:22:12 +0530

    Time: 1224 ms (5.01%) | Memory: 27 MB (49.64%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -3,21 +3,28 @@ public:
     int dp[2001][2001];
     bool solve(int i,int j,string& s,string& p){
         if(i>=s.length()){
-            if(j<p.length() && p[j]=='*') return solve(i,j+1,s,p);
-            else if(j<p.length() && p[j]!='*') return false;
+            if(j<p.length() && p[j]=='*') 
+                return solve(i,j+1,s,p);
+            else if(j<p.length() && p[j]!='*')
+                return false;
+
             return true;
         }
-        if(j>=p.length()) return false;
-        if(dp[i][j] != -1) return dp[i][j];
-        bool pc = false,q=false,c=false;
-        if(s[i] == p[j] || p[j]=='?') pc = solve(i+1,j+1,s,p);
-        else if(p[j] == '*'){
-            pc = solve(i+1,j,s,p);
-            q= solve(i+1,j+1,s,p);
-            c= solve(i,j+1,s,p);
-        }
 
-        return dp[i][j] = pc|q|c;
+        if(j>=p.length())
+            return false;
+            
+        if(dp[i][j] != -1)
+            return dp[i][j];
+
+        bool pc = false;
+        if(s[i] == p[j] || p[j]=='?') 
+            pc = solve(i+1,j+1,s,p);
+        else if(p[j] == '*')
+            pc = solve(i+1,j,s,p) | solve(i+1,j+1,s,p) | solve(i,j+1,s,p);
+
+        return dp[i][j] = pc;
+        
     }
     bool isMatch(string s, string p) {
         memset(dp,-1,sizeof(dp));
